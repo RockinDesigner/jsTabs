bf17c95e66d31728fa3ca4e7547374a87ac3cd92
RockinDesigner <43171022+RockinDesigner@users.noreply.github.com>
2019-04-02 04:00:20 +0300

    initial

---
+++ js/tabs.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+
+    $('body').on('click','.content-nav-group li a', function(){
+        $('.content-nav-group a').removeClass('active');
+        $(this).addClass('active');
+        var href = $(this).attr('href');
+        $('.tabs-content-item').removeClass('active');
+        $(href).addClass('active');
+        return false;
+    });
+    $('body').on('click','.slick-nav-group li a', function(){
+        $('.slick-nav-group a').removeClass('content-active');
+        $(this).addClass('content-active');
+        var href = $(this).attr('href');
+        $('.tabs-content-item').removeClass('content-active');
+        $(href).addClass('content-active');
+        return false;
+    });
+
+});
+
+$(function() {
+
+    var $el, leftPos, newWidth,
+        $mainNav = $("#slick-line");
+
+    $mainNav.append("<li id='magic-line'></li>");
+    var $magicLine = $("#magic-line");
+
+    $magicLine
+        .width($(".current_page_item").width())
+        .css("left", $(".current_page_item a").position().left)
+        .data("origLeft", $magicLine.position().left)
+        .data("origWidth", $magicLine.width());
+
+    $("#slick-line li a").hover(function() {
+        $el = $(this);
+        leftPos = $el.position().left;
+        newWidth = $el.parent().width();
+        $magicLine.stop().animate({
+            left: leftPos,
+            width: newWidth
+        });
+    }, function() {
+        $magicLine.stop().animate({
+            left: $magicLine.data("origLeft"),
+            width: $magicLine.data("origWidth")
+        });
+    });
+});
